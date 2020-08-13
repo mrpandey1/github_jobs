7d4a66cbdd294c1690b3658aca58d726e24e0afe
mrpandey1 <pandeynishant12345@gmail.com>
2020-08-13 13:06:38 +0530

    done with ui

--- src/useFetchJobs.js
+++ src/useFetchJobs.js
@@ -4,7 +4,8 @@ import axios from 'axios';
 const ACTIONS={
     MAKE_REQUEST:'make-request',
     GET_DATA:'get-data',
-    ERROR:'error'
+    ERROR:'error',
+    UPDATE_HAS_NEXT_PAGE:'update_has_next_page'
 }
 
 
@@ -18,6 +19,8 @@ function reducer(state,action){
             return {...state,loading:false,jobs:action.payload.jobs}
         case ACTIONS.ERROR:
             return {...state,loading:false,error:action.payload.error,jobs:[]}
+        case ACTIONS.UPDATE_HAS_NEXT_PAGE:
+            return {...state,hasNextPage:action.payload.hasNextPage}
         default:
             return state
     }
@@ -27,10 +30,11 @@ export default function useFetchJobs(params,page){
     const  [state,dispatch]=useReducer(reducer,{jobs:[],loading:true})
 
     useEffect(()=>{
-        const cancelToken=axios.CancelToken.source()
+        const cancelToken1=axios.CancelToken.source()
         dispatch({type:ACTIONS.MAKE_REQUEST})
+        
         axios.get(BASE_URL,{
-            cancelToken:cancelToken.token,
+            cancelToken:cancelToken1.token,
             params:{markdown:true,page:page,...params}
         }).then(res=>{
             dispatch({type:ACTIONS.GET_DATA,payload:{jobs:res.data}})
@@ -38,8 +42,21 @@ export default function useFetchJobs(params,page){
             if(axios.isCancel(e)) return 
             dispatch({type:ACTIONS.ERROR,payload:{error:e}})
         })
+
+
+        const cancelToken2=axios.CancelToken.source()
+        axios.get(BASE_URL,{
+            cancelToken:cancelToken2.token,
+            params:{markdown:true,page:page+1,...params}
+        }).then(res=>{
+            dispatch({type:ACTIONS.UPDATE_HAS_NEXT_PAGE,payload:{hasNextPage:res.data.lenght !==0}})
+        }).catch(e=>{
+            if(axios.isCancel(e)) return 
+            dispatch({type:ACTIONS.ERROR,payload:{error:e}})
+        })
         return()=>{
-            cancelToken.cancel()
+            cancelToken1.cancel()
+            cancelToken2.cancel()
         }
     },[params,page])
 
