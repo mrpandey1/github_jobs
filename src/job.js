4f3f8af249677d06a6b90e4e78400faa28cce451
mrpandey1 <pandeynishant12345@gmail.com>
2020-08-13 11:29:58 +0530

    done with getting json file and showing it as a component

--- src/job.js
+++ src/job.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import {Card} from 'react-bootstrap';
-
+import {Card,Badge,Button} from 'react-bootstrap';
+import ReactMarkdown from 'react-markdown';
 export default function Job({job}) {
     return (
         <Card>
@@ -10,8 +10,26 @@ export default function Job({job}) {
                         <Card.Title>
                             {job.title} - <span className="text-muted font-weight-light">{job.company}</span>
                         </Card.Title>
+                        <Card.Subtitle className="text-muted md-2">
+                            {new Date(job.created_at).toLocaleDateString()}
+                        </Card.Subtitle>
+                        <Badge variant="secondary" className='mr-2'>{job.type}</Badge>
+                        <Badge variant="secondary">{job.location}</Badge>
+                        <div style={{wordBreak:'break-all'}}>
+                            <ReactMarkdown source={job.how_to_apply}>
+                            </ReactMarkdown>
+                        </div>
                     </div>
+                    <img className="d-none d-md-block" height="50" alt={job.company} src={job.company_logo}/>
                 </div>
+                <Card.Text>
+                    <Button variant="primary">
+                        View Details
+                    </Button>
+                    <div className="mt-4">
+                        <ReactMarkdown source={job.description}></ReactMarkdown>
+                    </div> 
+                </Card.Text>
             </Card.Body>
         </Card>
     )
