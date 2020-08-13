a69b034612faab7ee27b2a84c33a7d42a4db0320
mrpandey1 <pandeynishant12345@gmail.com>
2020-08-13 11:18:13 +0530

    done with the github api

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React,{useState} from 'react';
 import useFetchJobs from './useFetchJobs';
 import {Container} from 'react-bootstrap';
+import Job from './job'
 function App(){
-  const {jobs,loading,error}=useFetchJobs()
+  const [params,setParams]=useState({})
+  const [page,setPage]=useState(1 )
+  const {jobs,loading,error}=useFetchJobs(params,page)
   
   return(
     <Container>
       {loading && <h1> Loading</h1>}
       {error  &&  <h1>Error try refreshing</h1>}
-      {<h1>{jobs.length}</h1>}
+      {jobs.map(job=>{
+        return <Job key={job.id} job={job}/>
+      })}
     </Container>
   )
 }
