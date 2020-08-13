af6d823f0644d2186b094aa9be3a160b0063876e
mrpandey1 <pandeynishant12345@gmail.com>
2020-08-13 20:00:03 +0530

    Updates

---
+++ precache-manifest.93de86fe999ac0cff9832f7c0afa5370.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "683ee7408ba04b7d456cf807425a4181",
+    "url": "/github_jobs/index.html"
+  },
+  {
+    "revision": "633e1b68ba2844e1c392",
+    "url": "/github_jobs/static/js/2.1ddf196d.chunk.js"
+  },
+  {
+    "revision": "8ebb70227fec3b61ad3af3d99ebe8940",
+    "url": "/github_jobs/static/js/2.1ddf196d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9fbe879614398689c29",
+    "url": "/github_jobs/static/js/main.706b93ff.chunk.js"
+  },
+  {
+    "revision": "54a21af5687a5a02feb3",
+    "url": "/github_jobs/static/js/runtime-main.a22786e1.js"
+  }
+]);
